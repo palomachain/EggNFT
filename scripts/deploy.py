52b77ab21f86c4f23c762341267d88102947de84
webelf101 <m63974529@gmail.com>
2022-08-30 03:55:35 +0800

    update compass-evm address

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,6 +2,6 @@ from ape import accounts, project
 
 def main():
     acct = accounts.load("deployer_account")
-    COMPASS_EVM = "0x"
+    COMPASS_EVM = "0x6D727250bd150A9dC006b65b6C7a0D817B02bB2e"
     contract = acct.deploy(project.Egg, COMPASS_EVM)
     return contract
